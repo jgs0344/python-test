c4b6d2f73b12bf1777c08a1c9b75b93a12b25745
jgs0344 <jgs0344@gmail.com>
2018-06-20 15:34:43 +0900

    assert 테스트

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
-import sys
 
-sys.
+def apply_discount(product, discount):
+    price = int(product['price'] * (1.0 - discount))
+    assert 0 <= price <= product['price']
+    return price
+
+shoes = {'name': 'Fancy Shoes', 'price': 14900}
+print(apply_discount(shoes, 2))
